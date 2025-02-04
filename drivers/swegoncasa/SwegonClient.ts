502441d4b2164cb3bf2c3cd24d8d1462cc77d3b0
philipostli <55027973+philipostli@users.noreply.github.com>
2025-02-05 00:44:56 +0100

    Fixed double logic on receiving 42 + Clear ping interval when socket closes

--- drivers/swegoncasa/SwegonClient.ts
+++ drivers/swegoncasa/SwegonClient.ts
@@ -30,6 +30,7 @@ class SwegonClient {
   private eventHandler: EventEmitter;
   private logger: Logger;
   private timestamp: Date = new Date();
+  private pingInterval: NodeJS.Timeout | null = null;
 
   constructor(username: string, password: string, logger: Logger) {
     this.username = username;
@@ -226,14 +227,12 @@ class SwegonClient {
         };
         this.logger.debug("Sending connect data:", connectData);
         ws.send(`40${JSON.stringify(connectData)}`);
-
-        // Set up subscription og read flyttes til etter vi mottar connect_error eller connect
         
         // Keep the connection alive with ping messages
-        setInterval(() => {
-          this.logger.debug("Sending ping");
+        this.pingInterval = setInterval(() => {
           ws.send('3');
-        }, 5000);
+          this.logger.debug("Sending ping");
+        }, 25000);
       });
 
       ws.on('error', (err: any) => {
@@ -245,6 +244,10 @@ class SwegonClient {
       });
 
       ws.on('close', (code: number, reason: Buffer) => {
+        if (this.pingInterval) {
+          clearInterval(this.pingInterval);
+          this.pingInterval = null;
+        }
         this.logger.error('WebSocket Closed:', {
           code,
           reason: reason.toString(),
@@ -258,22 +261,16 @@ class SwegonClient {
       });
 
       ws.on(
-        'message', () => {
-          this.logger.debug("Received message")
-        }
-      )
-
-      ws.on(
         'message',
         (data: Buffer | ArrayBuffer | Buffer[], isBinary: boolean) => {
           let value = data.toString();
-          console.log("Received raw message:", value);
+          // this.logger.debug('Received message from Swegon: ', value);
 
           // Håndter Socket.IO v4 handshake
           if (value.startsWith('0')) {
             try {
               const handshakeData = JSON.parse(value.substring(1));
-              console.log("Handshake data:", handshakeData);
+              console.debug("Handshake data:", handshakeData);
               
               // Send connect packet
               const connectData = {
@@ -288,24 +285,31 @@ class SwegonClient {
             }
           }
 
-          // Håndter heartbeat
-          if (value === '2') {
-            this.logger.debug('Received heartbeat');
-            ws.send('3');  // Send heartbeat response
-            return;
-          }
-
           // Håndter error meldinger
           if (value.startsWith('44')) {
             this.logger.error('Received error:', value);
             return;
           }
 
-          // Send read og subscribe etter vellykket tilkobling
           if (value.startsWith('42')) {
             try {
+
               const messageData = JSON.parse(value.substring(2));
-              if (messageData[1] && messageData[1].includes('device_connected')) {
+              const message = JSON.parse(messageData[1]);
+              //this.logger.debug('Parsed message content:', message);
+    
+              if (messageData[1] && message?.method === 'device_connected') { 
+
+                // Send read og subscribe etter vellykket tilkobling
+
+                const messageContent = (message as SwegonConnectionMessage).params;
+
+                this.eventHandler.emit('connectionInfo', {
+                  id: messageContent?.id,
+                  deviceName: messageContent?.devicename,
+                  serialNumber: messageContent?.serialnumber,
+                } as ConnectionInfo);
+
                 // Set up subscription og read
                 const subscription = [
                   "message",
@@ -321,13 +325,27 @@ class SwegonClient {
                 this.logger.debug("Sent read");
                 ws.send(`42${JSON.stringify(subscription)}`);
                 this.logger.debug("Sent subscription");
-              } else {
-                // Parse verdimeldinger
-                const messageContent = JSON.parse(messageData[1]);
-                this.logger.debug('Parsed message content:', messageContent);
 
-                if (messageContent.method === 'value' && messageContent.params?.objects) {
-                  messageContent.params.objects.forEach((obj: any) => {
+              } else if (
+                  message?.result &&
+                  (message as SwegonDeviceMessage).result.objects &&
+                  (message as SwegonDeviceMessage).result.objects.length > 0
+                ) {
+                  const messageContent = (message as SwegonDeviceMessage).result
+                    .objects[0];
+  
+                  this.eventHandler.emit('deviceInfo', {
+                    serialNumber:
+                      messageContent?.properties[SwegonPropertyType.ClimateMode]
+                        ?.value,
+                  } as DeviceInfo);
+                
+              } else {
+               
+              // Parse verdimeldinger
+
+                if (message.method === 'value' && message.params?.objects) {
+                  message.params.objects.forEach((obj: any) => {
                     this.logger.debug('Received value for object:', {
                       id: obj.id,
                       properties: obj.properties
@@ -460,170 +478,7 @@ class SwegonClient {
               this.logger.error('Unable to parse Swegon message', parseErr);
             }
           }
-
-          this.logger.debug('Received message from Swegon', value);
-
-          if (value.startsWith('42')) {
-            value = value.substring(2);
-
-            try {
-              const message = JSON.parse(value) as SwegonBaseMessage;
-
-              if (message?.method === 'value') {
-                const messageContent = (
-                  message as SwegonMeasurementMessage
-                ).params?.objects?.find(
-                  (x) => x.properties[SwegonPropertyType.Measurement] != null,
-                );
-                const measurementValue =
-                  messageContent?.properties[SwegonPropertyType.Measurement]
-                    ?.value || '0';
-
-                switch (messageContent?.id) {
-                  case SwegonObjectId.SupplyTemperature:
-                    this.eventHandler.emit('measurement', {
-                      type: MeasurementType.SupplyTemperature,
-                      value: parseFloat(measurementValue),
-                    });
-                    break;
-                  case SwegonObjectId.OutputTemperature:
-                    this.eventHandler.emit('measurement', {
-                      type: MeasurementType.ReturnTemperature,
-                      value: parseFloat(measurementValue),
-                    });
-                    break;
-                  case SwegonObjectId.IntakeTemperature:
-                    this.eventHandler.emit('measurement', {
-                      type: MeasurementType.IntakeTemperature,
-                      value: parseFloat(measurementValue),
-                    });
-                    break;
-                  case SwegonObjectId.Humidity:
-                    this.eventHandler.emit('measurement', {
-                      type: MeasurementType.HumidityPercent,
-                      value: parseFloat(measurementValue),
-                    });
-                    break;
-                  case SwegonObjectId.HumidityAmount:
-                    this.eventHandler.emit('measurement', {
-                      type: MeasurementType.HumidityAmount,
-                      value: parseFloat(measurementValue) / 10,
-                    });
-                    break;
-                  case SwegonObjectId.CurrentFanSpeed:
-                    this.eventHandler.emit('measurement', {
-                      type: MeasurementType.CurrentFanSpeed,
-                      value: parseInt(measurementValue, 10),
-                    });
-                    break;
-                  case SwegonObjectId.VentilationLevelIn:
-                    this.eventHandler.emit('measurement', {
-                      type: MeasurementType.VentilationLevelIn,
-                      value: parseInt(measurementValue, 10),
-                    });
-                    break;
-                  case SwegonObjectId.VentilationLevelOut:
-                    this.eventHandler.emit('measurement', {
-                      type: MeasurementType.VentilationLevelOut,
-                      value: parseInt(measurementValue, 10),
-                    });
-                    break;
-                  case SwegonObjectId.BoostCountDown:
-                    this.eventHandler.emit('measurement', {
-                      type: MeasurementType.BoostCountDown,
-                      value: parseInt(measurementValue, 10),
-                    });
-                    break;
-                  case SwegonObjectId.SetFanSpeed:
-                    this.eventHandler.emit('mode', {
-                      id: ModeType.ClimateMode,
-                      value: measurementValue,
-                    });
-                    break;
-                  case SwegonObjectId.SummerNightCoolingBoost:
-                    this.eventHandler.emit('setting', {
-                      id: SettingType.SummerNightCoolingBoost,
-                      value: measurementValue,
-                    });
-                    break;
-                  case SwegonObjectId.SummerNightCoolingMode:
-                    this.eventHandler.emit('mode', {
-                      id: ModeType.SummerNightCoolingMode,
-                      value: parseInt(measurementValue, 10),
-                    });
-                    break;
-                  case SwegonObjectId.TemperatureControlMode:
-                    this.eventHandler.emit('setting', {
-                      id: SettingType.TemperatureControlMode,
-                      value: measurementValue,
-                    });
-                    break;
-                  case SwegonObjectId.AutoHumidityControlMode:
-                    this.eventHandler.emit('mode', {
-                      id: ModeType.AutoHumidityControlMode,
-                      value: parseInt(measurementValue, 10),
-                    });
-                    break;
-                  case SwegonObjectId.AutoHumidityControlBoostLimit:
-                    this.eventHandler.emit('setting', {
-                      id: SettingType.AutoHumidityControlBoostLimit,
-                      value: parseInt(measurementValue, 10),
-                    });
-                    break;
-                  case SwegonObjectId.AutoHumidityControlFullBoostLimit:
-                    this.eventHandler.emit('setting', {
-                      id: SettingType.AutoHumidityControlFullBoostLimit,
-                      value: parseInt(measurementValue, 10),
-                    });
-                    break;
-                  case SwegonObjectId.TravellingModeTemperatureDrop:
-                    this.eventHandler.emit('setting', {
-                      id: SettingType.TravellingModeTemperatureDrop,
-                      value: parseInt(measurementValue, 10),
-                    });
-                    break;
-                  case SwegonObjectId.AwayModeTemperatureDrop:
-                    this.eventHandler.emit('setting', {
-                      id: SettingType.AwayModeTemperatureDrop,
-                      value: parseInt(measurementValue, 10),
-                    });
-                    break;
-                  case SwegonObjectId.SupplyTemperatureSetpoint:
-                    this.eventHandler.emit('setting', {
-                      id: SettingType.SupplyTemperatureSetpoint,
-                      value: parseInt(measurementValue, 10),
-                    });
-                    break;
-                  default:
-                    break;
-                }
-              } else if (message?.method === 'device_connected') {
-                const messageContent = (message as SwegonConnectionMessage)
-                  .params;
-
-                this.eventHandler.emit('connectionInfo', {
-                  id: messageContent?.id,
-                  deviceName: messageContent?.devicename,
-                  serialNumber: messageContent?.serialnumber,
-                } as ConnectionInfo);
-              } else if (
-                message?.result &&
-                (message as SwegonDeviceMessage).result.objects &&
-                (message as SwegonDeviceMessage).result.objects.length > 0
-              ) {
-                const messageContent = (message as SwegonDeviceMessage).result
-                  .objects[0];
-
-                this.eventHandler.emit('deviceInfo', {
-                  serialNumber:
-                    messageContent?.properties[SwegonPropertyType.ClimateMode]
-                      ?.value,
-                } as DeviceInfo);
-              }
-            } catch (parseErr) {
-              this.logger.error('Unable to parse Swegon message', parseErr);
-            }
-          }
+     
         },
       );
 
