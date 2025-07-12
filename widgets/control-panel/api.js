71763ca8210186db9ef62024dba9b81cbc5a0be9
philipostli <55027973+philipostli@users.noreply.github.com>
2025-07-13 00:02:38 +0200

    Added widget

---
+++ widgets/control-panel/api.js
@@ -0,0 +1,36 @@
+'use strict';
+
+module.exports = {
+  async getUnitState({ homey, query }) {
+
+    const selectedDeviceId = query.deviceId;
+    const driver = await homey.drivers.getDriver('swegoncasa');
+    const devices = driver.getDevices();
+    const selectedDevice = devices.find(device => device.getId() === selectedDeviceId);
+    const name = await selectedDevice.getName();
+
+    let state = await selectedDevice.getCapabilityValue('climate_mode');
+    let fanSpeed = await selectedDevice.getCapabilityValue('measure_ventilation_level_in');
+    let humidityPercent = await selectedDevice.getCapabilityValue('measure_humidity_percent');
+    let intakeTemperature = await selectedDevice.getCapabilityValue('measure_intake_temperature');
+    let returnTemperature = await selectedDevice.getCapabilityValue('measure_return_temperature');
+
+    return {
+      name,
+      state,
+      fanSpeed,
+      humidityPercent,
+      intakeTemperature: intakeTemperature ? Number(intakeTemperature).toFixed(1) : null,
+      returnTemperature: returnTemperature ? Number(returnTemperature).toFixed(1) : null
+    };
+  },
+  async setClimateMode({ homey, query, body }) {
+    const selectedDeviceId = query.deviceId;
+    const driver = await homey.drivers.getDriver('swegoncasa');
+    const devices = driver.getDevices();
+    const selectedDevice = devices.find(device => device.getId() === selectedDeviceId);
+
+    await selectedDevice.setCapabilityValue('climate_mode', body.climateMode);
+    await selectedDevice.triggerCapabilityListener('climate_mode', body.climateMode);
+  }
+};
